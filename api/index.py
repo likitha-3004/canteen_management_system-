0d1701fc83c3dc4727d45384f11e49fa62e60c34
tejaswini reddy middepalle <190929818+likitha-3004@users.noreply.github.com>
2025-11-04 10:22:37 +0530

    first commit

--- api/index.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-# Add repo root to sys.path so we can import backend.app
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-
-from backend.app import app  # your Flask instance is named `app`
